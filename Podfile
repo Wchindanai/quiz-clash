38417af1ee79334991ee085a411639b2f80d5bc5
Chindanai Wasanachin <dn.chindanai@gmail.com>
2017-05-17 23:14:51 +0700

    init develop

--- Podfile
+++ Podfile
@@ -1,9 +1,12 @@
 # Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
-pod 'Firebase/Core'
+platform :ios, '10.0'
+
 target 'QuizClash' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
+  pod 'Firebase/Core'
+  pod 'Firebase/Database'
+  pod 'Firebase/Storage'
 
   # Pods for QuizClash
 
---
+++ QuizClash/RegisterController.swift
@@ -0,0 +1,38 @@
+//
+//  RegisterController.swift
+//  QuizClash
+//
+//  Created by Chindanai Wasanachin on 5/17/2560 BE.
+//  Copyright © 2560 Adirek. All rights reserved.
+//
+
+import UIKit
+
+class RegisterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func Register(_ sender: UIButton) {
+        print("HelloWorld")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
