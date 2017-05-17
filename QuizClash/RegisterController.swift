583d4dbc5de4379eaeed9f4fd1a1c81f08e04417
Chindanai Wasanachin <dn.chindanai@gmail.com>
2017-05-18 01:28:20 +0700

    login and register to firebase

--- QuizClash/RegisterController.swift
+++ QuizClash/RegisterController.swift
@@ -7,9 +7,21 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+
 
 class RegisterController: UIViewController {
 
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var firstname: UITextField!
+    @IBOutlet weak var lastname: UITextField!
+    
+    
+    var ref: FIRDatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,18 +33,76 @@ class RegisterController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func checkEmail() -> Bool{
+        if email.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please type your email", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            present(alertController, animated: true, completion: nil)
+            return false
+        }
+        else{
+            return true
+        }
+    }
+    
+    func checkPassword() -> Bool{
+        if password.text == "" {
+        
+            let alertController = UIAlertController(title: "Error", message: "Please type your password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            present(alertController, animated: true, completion: nil)
+            return false
+        }
+        else{
+            return true
+        }
+    }
+    
+    func changeProfileMethod(){
+        let uid = FIRAuth.auth()?.currentUser?.uid
+        
+        ref = FIRDatabase.database().reference()
+        
+        self.ref.child("users").child(uid!).setValue(["name": "\(firstname.text!) \(lastname.text!)"])
+
+        
+    }
+    
     @IBAction func Register(_ sender: UIButton) {
-        print("HelloWorld")
+        if !checkEmail(){
+            return
+        }
+        if !checkPassword() {
+            return
+        }
+        
+        FIRAuth.auth()?.createUser(withEmail: email.text!, password: password.text!) { (user, error) in
+            
+            if error == nil {
+                print("You have successfully signed up")
+                
+                self.changeProfileMethod()
+                
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "SignIn")
+                self.present(vc!, animated: true, completion: nil)
+                
+            } else {
+                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+        
+       
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
 
 }
--- QuizClash/ViewController.swift
+++ QuizClash/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
     
 
     override func viewDidLoad() {
@@ -30,7 +32,56 @@ class ViewController: UIViewController {
         
     }
     
+    func startSpinner(){
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.color = UIColor.cyan
+        view.addSubview(activityIndicator)
+        
+        activityIndicator.startAnimating()
+    }
+    
+    func stopSpinner() {
+        activityIndicator.stopAnimating()
+    
+    }
+    
     @IBAction func SignInMethod(_ sender: Any) {
+        if email.text == "" && password.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please type your email Or password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            return
+        }
+        else{
+            startSpinner()
+            FIRAuth.auth()?.signIn(withEmail: email.text!, password: password.text!) { (FIRUser, Error) in
+                
+                if Error == nil {
+                    //Print into the console if successfully logged in
+                    print("You have successfully logged in")
+                    
+                    //Go to the HomeViewController if the login is sucessful
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.present(vc!, animated: true, completion: nil)
+                    self.stopSpinner()
+                }
+                else{
+                    //Tells the user that there is an error and then gets firebase to tell them the error
+                    let alertController = UIAlertController(title: "Error", message: Error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+                
+            }
+
+        }
     }
 
 
